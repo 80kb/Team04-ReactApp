ecb403ee91fc4e734a6de18b7143dbfce05f3c1e
J Henning <jonathanchenning@outlook.com>
2024-10-08 13:01:33 -0400

    Added comments

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import $ from 'jquery';
 
 class About extends Component {
+
+	// Initializes the state to default values.
+	// That's all this is really needed for at the moment.
 	constructor(props) {
 		super(props);
 
@@ -10,20 +13,36 @@ class About extends Component {
 		};
 	}
 
+	// Runs when the component is loaded.
+	// In this case the "component" is the entire about page.
+	//
+	// This function will make calls to the API and update
+	// global vairables with the results from the api call.
+	//
+	// The global variables are stored in the this.state dictionary
+	// as seen in the constructor above. The constructor is only
+	// setting the initial default state values, and this function will
+	// update those values with the values we receive from the api.
 	componentDidMount() {
 		$.ajax({
-			url: 'https://reqres.in/api/users?page=2',
-			type: 'GET',
+			url: 'https://reqres.in/api/users?page=2',		// URL for the api call, just copy from the browser
+			type: 'GET',						// The type of API call
 			dataType: 'json',
 			success: (data) => {
-				this.setState({ teamnum: data['per_page'] });
+				this.setState({ teamnum: data['per_page'] }); 	// Update the state to the data received (if successful)
 			},
 			error: (xhr, stat, err) => {
-				console.error('Error: ', err);
+				console.error('Error: ', err);			// Print the error if the api call failed
 			}
 		});
 	}
 
+	// The function that displays the html for the page.
+	// Runs after componentDidMount() is run.
+	//
+	// Uses the global state values given by the API calls in componentDidMount()
+	// if the API calls were successful. Otherwise it will display the default value
+	// defined in the constructor.
 	render() {
 		return (
 			<>
