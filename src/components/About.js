57a7066c138636976de9fe7d31a1642ad8ae10eb
J Henning <jonathanchenning@outlook.com>
2024-10-08 12:47:13 -0400

    Begin making API calls

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react';
+import $ from 'jquery';
 
-function About() {
-	return <h1>About Us</h1>
+class About extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			teamnum: 2,
+		};
+	}
+
+	componentDidMount() {
+		$.ajax({
+			url: 'https://reqres.in/api/users?page=2',
+			type: 'GET',
+			dataType: 'json',
+			success: (data) => {
+				this.setState({ teamnum: data['per_page'] });
+			},
+			error: (xhr, stat, err) => {
+				console.error('Error: ', err);
+			}
+		});
+	}
+
+	render() {
+		return (
+			<>
+			<h1>About Us</h1>
+			<ul>
+			<li>{this.state.teamnum}</li>
+			</ul>
+			</>
+		);
+	}
 }
 
 export default About;
