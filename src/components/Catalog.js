a245e8fff50bb31b4e94a2ca43fbcc2b25b4cecb
J Henning <jonathanchenning@outlook.com>
2024-10-22 12:31:29 -0400

    module changes

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,8 +1,7 @@
-
 const Catalog = () => (
 	<div className='catalog'>
-		<h1>Driver Reward Catalog</h1>
-		<p>Call your sponsor for more details at 1800-123-5555</p>
+	<h1>Driver Reward Catalog</h1>
+	<p>Call your sponsor for more details at 1800-123-5555</p>
 	</div>
 );
 
