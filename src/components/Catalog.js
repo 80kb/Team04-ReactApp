6e22ec0e32a8fd9e7c656e9c329e48b20f64962b
J Henning <jonathanchenning@outlook.com>
2024-10-23 11:50:58 -0400

    Implemented search function for catalog

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -8,21 +8,28 @@ class Catalog extends Component {
 
 		this.state = {
 			productimage: '',
-			productname: ''
+			productname: '',
+			productprice: '',
+			searchQuery: ''
 		};
 	}
 
-	componentDidMount() {
+	handleChange = (event) => {
+		this.setState({ searchQuery: event.target.value });
+	}
+
+	handleSubmit = (event) => {
 		$.ajax({
-			url: 'https://ckszaimc23.execute-api.us-east-2.amazonaws.com/EBAYAPIDepolyment1/ebay-search',		// URL for the api call, just copy from the browser
+			url: 'https://ckszaimc23.execute-api.us-east-2.amazonaws.com/EBAYAPIDepolyment1/ebay-search?search=' + this.state.searchQuery,		// URL for the api call, just copy from the browser
 			type: 'POST',						// The type of API call
 			dataType: 'json',
 			success: (data) => {
 
-				var searchResult = data['findItemsAdvancedResponse']['0']['searchResult']['0'];
+				var searchResult = data['findItemsAdvancedResponse']['0']['searchResult']['0']['item'];
 				this.setState({
-					productimage: searchResult['item']['0']['galleryURL'],
-					productname: searchResult['item']['0']['title'],
+					productimage: searchResult['0']['galleryURL'],
+					productname: searchResult['0']['title'],
+					productprice: searchResult['0']['sellingStatus']['0']['currentPrice']['0']['__value__'],
 				}); 	// Update the state to the data received (if successful)
 
 				console.log(this.state.productimage);
@@ -39,8 +46,12 @@ class Catalog extends Component {
 			<h1>Driver Reward Catalog</h1>
 			<p>Call your sponsor for more details at 1800-123-5555</p>
 
+			<input type="text" onChange={this.handleChange} />
+			<button type="button" onClick={this.handleSubmit}>Search</button>
+
 			<h2>Items</h2>
 			<p>Title: {this.state.productname}</p>
+			<p>Price: {this.state.productprice}</p>
 			<img src={this.state.productimage}/>
 			</div>
 		);
