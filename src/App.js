d93296bffc57e05cbdee98324bdb9e291a0a39dc
Kevin Lin <kevinllin221@gmail.com>
2024-11-11 11:31:35 -0500

    SignOut Button is created to sign current logged in users out

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Authenticator } from '@aws-amplify/ui-react';
 import Navigation from './components/Navigation';
 import Main from './components/Main';
+import './App.css';
+import { Amplify } from 'aws-amplify';
+import outputs from './amplify_outputs.json';
+
+Amplify.configure(outputs);
 
 function App() {
   return (
-    <div className="app-container">
-      <header className="app-header">
-        <h1>Driver Rewards</h1>
-      </header>
-      <Navigation />
-      <main className="app-main">
-        <Main />
-      </main>
-      <footer className="app-footer">
-        <p>&copy; Driver Rewards, Inc.</p>
-      </footer>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="app-container">
+          <header className="app-header">
+            <h1>Driver Rewards</h1>
+          </header>
+          <Navigation user={user} signOut={signOut} />
+          <main className="app-main">
+            <Main />
+          </main>
+          <footer className="app-footer">
+            <p>&copy; Driver Rewards, Inc.</p>
+          </footer>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
 export default App;
-
--- src/components/LoginSignup.js
+++ src/components/LoginSignup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/LoginSignup.css';
+<<<<<<< Updated upstream
 
 const LoginSignup = () => {
     return (
@@ -13,4 +14,71 @@ const LoginSignup = () => {
     );
 };
 
+=======
+import { Authenticator, SignOut } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(outputs);
+
+const LoginSignup = () => {
+    return (
+        <Authenticator formFields={formFields}>
+            {({ signOut, user }) => (
+                <div className="login-signup-container">
+                    <p>Welcome, {user ? user.username : "please choose an option below:"}</p>
+                    <div className="button-container">
+                        {user && (
+                            <button className="auth-button" onClick={signOut}>Sign Out</button>
+                        )}
+                    </div>
+                </div>
+            )}
+        </Authenticator>
+    );
+};
+
+const formFields = {
+    signUp: {
+      given_name: {
+        order:1,
+        label:'First Name',
+        required:true
+      },
+      family_name: {
+        order: 2,
+        label:'Last Name',
+        required:true
+      },
+      email: {
+        order: 3,
+        placeholder: 'Enter your Email',
+        required:true
+      },
+      preferred_username: {
+        order: 4,
+        placeholder: 'Enter username',
+        required:true
+      },
+      password: {
+        order: 5,
+        placeholder: 'Enter your desired password',
+        required: true
+      },
+      confirm_password: {
+        order: 6,
+        placeholder: 'Enter the same password',
+        required: true
+      },
+      address: {
+        order: 7,
+        label: 'Address',
+        placeholder: 'Enter address',
+        required: true
+      },
+    },
+}
+
+>>>>>>> Stashed changes
 export default LoginSignup;
