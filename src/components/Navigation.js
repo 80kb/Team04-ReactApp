02accd6b4961470f1ca7953373415a782911c0c6
Kevin Lin <kevinllin221@gmail.com>
2024-12-01 22:25:47 -0500

    Home Page Change, Reports Navbar removed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -16,7 +16,6 @@ const Navigation = () => {
 		  <li><NavLink to="/dashboard">Dashboard</NavLink></li>
 		  <li><NavLink to="/about">About</NavLink></li>
 		  <li><NavLink to="/catalog">Catalog</NavLink></li>
-		  <li><NavLink to="/reports">Reports</NavLink></li>
   
 		  {/* Conditionally render Sign Out or Login/Signup */}
 		  {user ? (
